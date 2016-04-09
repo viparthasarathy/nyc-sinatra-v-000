77bca758019302b1a8e69499445d78e6e2f2e682
viparthasarathy <viparthasarathy@gmail.com>
2016-04-09 00:06:11 -0700

    complete new and index views and controller actions

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,19 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    @figure.landmarks << Landmark.create(params[:landmark]) unless params[:landmark][:name].empty?
+    @figure.titles << Title.create(params[:title]) unless params[:title][:name].empty?
+    @figure.save
+    redirect to '/figures'
+  end
 end

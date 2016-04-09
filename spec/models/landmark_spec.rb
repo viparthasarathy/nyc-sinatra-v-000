06281df5bcfc00f09e567c672a039b726470490e
viparthasarathy <viparthasarathy@gmail.com>
2016-04-08 23:43:42 -0700

    create figure, landmark, and title models

--- spec/models/landmark_spec.rb
+++ spec/models/landmark_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
 describe "Landmark" do
-  before do 
-    @figure = Figure.create(:name => "Fiorello LaGuardia") 
+  before do
+    @figure = Figure.create(:name => "Fiorello LaGuardia")
 
-    @airport =  Landmark.create(:name => "LG Airport", :year_completed => 1950) 
+    @airport =  Landmark.create(:name => "LG Airport", :year_completed => 1950)
     @library = Landmark.create(:name => "Library")
-    
+
   end
 
 
-  it "has a name and a year_completed" do 
+  it "has a name and a year_completed" do
     expect(@airport.name).to eq("LG Airport")
     expect(@airport.year_completed).to eq(1950)
   end
 
-  it "belongs to a figure" do 
+  it "belongs to a figure" do
     @figure.landmarks << @airport
     expect(@airport.figure).to eq(@figure)
   end
